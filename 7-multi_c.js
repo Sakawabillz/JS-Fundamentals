7878bcc02118317fc09b618d9cc90570dc4a17d8
paschal <paschal>
2025-05-24 07:15:45 -0700

    Fix handling of negative numbers and missing argument cases

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -11,16 +11,3 @@ if (isNaN(number) || number <= 0) {
   }
   console.log(output.trim());
 }
-const number = parseInt(process.argv[2]);
-
-if (isNaN(number) || number <= 0) {
-  console.log("Missing number of occurrences");
-} else {
-  let i = 0;
-  let output = "";
-  while (i < number) {
-    output += "C is fun\n";
-    i++;
-  }
-  console.log(output.trim());
-}
